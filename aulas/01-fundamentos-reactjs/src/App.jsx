97ac85018e0b949fc1575deeef1ee500b880e15c
Dario Reis <guttyerresjunior1995@gmail.com>
2024-12-02 21:43:03 -0300

    Iniciando com React - Propriedades

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -3,12 +3,14 @@ import { Post } from "./Post"
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Dario Reis"
+        content="esse é um post Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque corrupti aperiam dolorum. Minima non, sunt laboriosam repudiandae atque tenetur corrupti laudantium porro dolore ab. Sit, laudantium impedit? Ea, voluptatem illo?"
+      />
+      <Post
+        author="Felipe Cardoso"
+        content="esse é outro post Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque corrupt"
+      />
     </div>
   )
 }
-
--- aulas/01-fundamentos-reactjs/src/Post.jsx
+++ aulas/01-fundamentos-reactjs/src/Post.jsx
@@ -1,6 +1,10 @@
-export function Post() {
+export function Post(props) {
     return (
-        <p>Post</p>
+        <>
+            <strong>{props.author}</strong>
+            <p>{props.content}</p>
+        </>
+
     );
 }
 
